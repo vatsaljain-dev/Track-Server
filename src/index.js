87d6ff94070c95e1c36dece991dab3dd14a8f8e8
vatsaljain-dev <vatsaljain.dev@gmail.com>
2025-09-11 01:03:00 +0530

    setup mongodb and connected mongodb

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
+
+require("dotenv").config();
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+
+mongoose.connection.on("connected", () => {
+  console.log("Connected to MongoDB");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.log("Error connecting to MongoDB:", err);
+});
 
 app.get("/", (req, res) => {
   res.send("Hi There");
