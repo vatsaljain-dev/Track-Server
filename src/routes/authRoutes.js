01a8f9bf0a63db6586e09edd64f0ab2cc270e0ee
vatsaljain-dev <vatsaljain.dev@gmail.com>
2025-09-11 19:07:30 +0530

    created authRoute - login and signup

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,10 +1,44 @@
 const express = require("express");
+const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
 const router = express.Router();
 
-router.post("/signup", (req, res) => {
-  console.log("req.body:", req.body);
-  res.send("User signed up");
+router.post("/signup", async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = new User({ email, password: hashedPassword });
+    await user.save();
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+    res.send({
+      token,
+    });
+  } catch (error) {
+    return res.status(400).send("Error signing up user: " + error.message);
+  }
+});
+
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user) return res.status(400).send("Invalid email or password");
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(400).send("Invalid email or password");
+
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+
+    res.send({ token });
+  } catch (error) {
+    res.status(500).send("Error logging in user: " + error.message);
+  }
 });
 
 module.exports = router;
